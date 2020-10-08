f9f3e0e46b50acdc8e705408bd9c675e00e4700d
Felix Oppong Willborg <felixwillborg@MacBook-Pro.local>
2020-10-08 10:09:21 +0200

    Added all images to app.jsx all test pass

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div>
+      <img src="/data/images/maner.png" id="maner" alt="Maner" data-cy="image" />
+      <img src="/data/images/blabar.png" id="blabar" alt="Blåbär" data-cy="image" />
+      <img src="/data/images/splash.svg" id="splash" alt="Splash" data-cy="image" />
     </div>
   );
 }
