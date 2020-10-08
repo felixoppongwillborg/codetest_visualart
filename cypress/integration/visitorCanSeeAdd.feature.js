459e769daffe725e5fd98b6b3b37b6fd28dfdf0c
Felix Oppong Willborg <felixwillborg@MacBook-Pro.local>
2020-10-08 12:04:15 +0200

    added second test to see al the info regarding the product and did a get request from the json file

---
+++ cypress/integration/visitorCanSeeAdd.feature.js
@@ -0,0 +1,20 @@
+describe('Visitor can see add campaign', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('visitor can see images', () => {
+    cy.get('#maner').should('exist')
+    cy.get('#blabar').should('exist')
+    cy.get('#splash').should('exist')
+  });
+  it('visitor can see text in add', () => {
+    cy.get('#layout').should('exist')
+    cy.get('#promotionUnit').should('exist')
+    cy.get('#subUnit').should('exist')
+    cy.get('#unitMesurement').should('exist')
+    cy.get('#articleName').should('exist')
+    cy.get('#brandName').should('exist')
+    cy.get('#originCountry').should('exist')
+    cy.get('#packageInfo').should('exist')
+  })
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 
 function App() {
-
-  const [adData, setAdData] = useState({})
-
+  debugger
+  const [adData, setAdData] = useState({});
   const getAdData = async () => {
-    const response = await axios.get('./data/Advdata.json')
-    setAdData(response.data)
-  }
+    const response = await axios.get("./data/Advdata.json");
+    setAdData(response.data);
+  };
 
   useEffect(() => {
-   getAdData()
-  }, [])
+    getAdData();
+  }, []);
 
   return (
     <div>
-      <img src="/data/images/maner.png" id="maner" alt="Maner" data-cy="image" />
-      <img src="/data/images/blabar.png" id="blabar" alt="Blåbär" data-cy="image" />
-      <img src="/data/images/splash.svg" id="splash" alt="Splash" data-cy="image" />
+      <div id="layout">{adData.layout}</div>
+      <div id="promotionUnit">{adData.promotion_unit}</div>
+      <div id="subUnit">{adData.promotion_sub_unit}</div>
+      <div id="unitMesurement">{adData.article_unit_of_measurement}</div>
+      <div id="articleName">{adData.article_name}</div>
+      <div id="brandName">{adData.article_brand_name}</div>
+      <div id="originCountry">{adData.article_origin_country}</div>
+      <div id="packageInfo">{adData.article_package_info}</div>
+      <img
+        src="/data/images/maner.png"
+        id="maner"
+        alt="Maner"
+        data-cy="image"
+      />
+      <img
+        src="/data/images/blabar.png"
+        id="blabar"
+        alt="Blåbär"
+        data-cy="image"
+      />
+      <img
+        src="/data/images/splash.svg"
+        id="splash"
+        alt="Splash"
+        data-cy="image"
+      />
     </div>
   );
 }
